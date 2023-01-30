4180af54ce591d83bf16be7c8dadf1c47d45ced2
SectumSempra99 <kennyrmdhn@gmail.com>
2023-01-31 02:46:38 +0700

    Configuration Environtment

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  env:{  
+    url : "https://rahulshettyacademy.com/"
+
+  },
+  projectId: "a9ya15",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
