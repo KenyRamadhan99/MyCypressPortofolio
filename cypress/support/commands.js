54cd268fd180f73a30ff8176eeb312f907d25fdd
SectumSempra99 <kennyrmdhn@gmail.com>
2023-01-31 02:47:39 +0700

    Custom Command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,8 +10,40 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+ Cypress.Commands.add('product', (productName) => {
+    cy.get('h4.card-title').each((el, index , list) => 
+{
+    if(el.text().includes(productName))
+    {
+        cy.get('button,btn.btn-info').eq(index).click()
+    }
+})
+  })
+
+  Cypress.Commands.add('amount', () => {
+    cy.get('tr td:nth-child(4) > strong').each((el, index , list) => 
+{
+    const amount = el.text()
+    var res = amount.split(" ")
+    res = res[1].trim()
+    sum = Number(sum)+Number(res)
+}).then(function(){
+    cy.log(sum)
+})
+  })
+
+  Cypress.Commands.add('totalAmount', () => {
+    cy.get('tr td:nth-child(4) > strong').each((el, index , list) => 
+{
+    const amount = el.text()
+    var res = amount.split(" ")
+    res = res[1].trim()
+    expect(Number(res)).to.equal(sum)
+})
+  })
 //
+
+
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
