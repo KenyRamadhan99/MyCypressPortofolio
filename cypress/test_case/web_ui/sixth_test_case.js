5a0ae99595f1d44ef17d6d69387c6d943f60a014
SectumSempra99 <kennyrmdhn@gmail.com>
2023-01-31 02:49:46 +0700

    Get Invisible Elements

--- cypress/test_case/web_ui/sixth_test_case.js
+++ cypress/test_case/web_ui/sixth_test_case.js
@@ -1,21 +1,29 @@
+import AutomationPractice from "../../PageObject/AutomationPractice"
 describe('My Second Test Suite', function() 
 {
  
 it('Test Case 6',function() {
  
 
-cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+cy.visit(Cypress.env('url')+"AutomationPractice/")
  // Ada 2 Cara untuk meng handle element yang invisible
 // pertama ,{force: true} digunakan untuk mem by pass element yang tidak terlihat 
 // kedua menggunakan jquery method show() yang mana akan menampikan elemen invisible 
 //cy.get('.mouse-hover-content').invoke('show')
-if (cy.contains('Top').click({force: true}))
+const automationPractice = new AutomationPractice()
+
+if (automationPractice.getMouseHover().click({force: true}))
 {
     cy.url().should('include','top')
 } else {
     print('Failed')
 }
 
+automationPractice.getTabButton().then(function(el)
+{
+    const url=el.prop('href')
+    cy.log(url)
+})
 
 
  
