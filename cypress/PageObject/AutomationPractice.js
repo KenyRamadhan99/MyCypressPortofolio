c92fabf5b20948091ffb5a27dce0ccaf0e2cc996
SectumSempra99 <kennyrmdhn@gmail.com>
2023-02-08 22:28:36 +0700

    Page Object

--- cypress/PageObject/AutomationPractice.js
+++ cypress/PageObject/AutomationPractice.js
@@ -1,5 +1,49 @@
 class AutomationPractice
 {
+    getAlert()
+    {
+        return  cy.get('#alertbtn')
+    }
+    getNewTab()
+    {
+        return cy.get('#opentab')
+    }
+    getRadioButton()
+    {
+        return cy.get('[value="radio2"]')
+    }
+    getShowText()
+    {
+        return cy.get('#show-textbox')
+    }
+    getHideTextBox()
+    {
+        return cy.get('#hide-textbox')
+    }
+    getDisplayedText()
+    {
+        return cy.get('#displayed-text')
+    }
+    getListDropdownDynamic()
+    {
+        return cy.get('.ui-menu-item div')
+    }
+    getDropdownDynamic()
+    {
+        return cy.get('#autocomplete')
+    }
+    getDropdownStatic()
+    {
+        return cy.get('select')
+    }
+    getAllCheckBox()
+    {
+        return cy.get('input[type="checkbox"]')
+    }
+    getCheckBox1()
+    {
+        return cy.get('#checkBoxOption1')
+    }
     getMouseHover()
     {
         return cy.contains('Top')
@@ -20,6 +64,7 @@ class AutomationPractice
     {
         return cy.iframe().find("h1[class*='pricing-title']")
     }
+    
 
 }
 export default AutomationPractice
